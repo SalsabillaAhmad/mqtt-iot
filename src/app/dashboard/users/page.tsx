7b53fd94029efa819585b6057db810933f203f05
SalsabillaAhmad <kellycielly22@gmail.com>
2025-07-21 12:12:14 +0700

    membuat tombol add subscriber dan add publisher

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -3,11 +3,28 @@
 import NavbarUser from "@/component/NavbarUser/page";
 import React from "react";
 
+
 export default function UsersDashboard() {
+  
   return (
-    <div className="min-h-screen bg-amber-500">
-      <NavbarUser/>
-      <main></main>
+    <div className="min-h-screen bg-amber-50">
+      <NavbarUser />
+      <main className="p-6">
+        <div className="space-x-4 mb-4">
+          <button
+            className="px-4 py-2 bg-blue-400 text-white rounded hover:bg-green-500"
+            onClick={()=>alert("ini tombol add subscriber sudah ditekan")}
+          >
+            Add Subscriber
+          </button>
+          <button
+            className="px-4 py-2 bg-blue-400 text-white rounded hover:bg-green-500"
+            onClick={()=>alert("ini tombol add publisher sudah ditekan")}
+          >
+            Add Publisher
+          </button>
+        </div>
+      </main>
     </div>
   );
 }

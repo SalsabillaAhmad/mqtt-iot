06822fdfa650fd618079ef79bfec9df8f663d26b
SalsabillaAhmad <kellycielly22@gmail.com>
2025-07-25 22:58:05 +0700

    membuat logika untuk menampilkan masing-masing komponen berdasarkan selected subscriber dan publisher

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-
 import { useDeviceManager } from "@/app/hooks/useDeviceManager";
 import NavbarUser from "@/component/NavbarUser/page";
 import React from "react";
 import AddButtons from "@/component/ui/AddButtons";
 import ButtonSubscriber from "@/component/ui/ButtonsSubscriber";
 import ButtonPublisher from "@/component/ui/ButtonPublisher";
-
 
 export default function UsersDashboard() {
   const {
@@ -22,17 +20,58 @@ export default function UsersDashboard() {
     <div className="min-h-screen bg-amber-50">
       <NavbarUser />
       <main className="p-6">
-        {!view && (
-      <AddButtons setView={setView}/>
+        {!view && <AddButtons setView={setView} />}
+        {view === "subscriber" && !selectedSubscriber && (
+          <ButtonSubscriber
+            setSelectedSubscriber={setSelectedSubscriber}
+            setView={setView}
+          />
         )}
-        {view === "subscriber" && (
-        <ButtonSubscriber setSelectedSubscriber ={setSelectedSubscriber} setView={setView}/>
+        {view === "publisher" && !selectedPublisher && (
+          <ButtonPublisher
+            setSelectedPublisher={setSelectedPublisher}
+            setView={setView}
+          />
         )}
-        {view === "publisher" && (
-        <ButtonPublisher setSelectedPublisher = {setSelectedPublisher} setView={setView}/>
+        {selectedSubscriber === "Subscriber Web" && (
+          <div>ini Komponen Subscriber web</div>
         )}
-        <div
-        className="bg-purple-400">
+        {selectedSubscriber === "Subscriber Device IoT" && (
+          <div>ini Komponen Subscriber Device IoT</div>
+        )}
+        {selectedPublisher === "Text" && 
+        <div>ini Komponen Publisher Text</div>}
+        {selectedPublisher === "Button" && (
+          <div>ini Komponen Publisher Button</div>
+        )}
+        {selectedPublisher === "Switch" && (
+          <div>ini Komponen Publisher Switch</div>
+        )}
+        {selectedPublisher === "Seek Bar" && (
+          <div>ini Komponen Publisher Seek bar </div>
+        )}
+        {selectedPublisher === "Combo Box" && (
+          <div>ini Komponen Publisher Combo Box </div>
+        )}
+        {selectedPublisher === "Color Picker" && (
+          <div>ini Komponen Publisher Color Picker </div>
+        )}
+        {selectedPublisher === "Time Picker" && (
+          <div>ini Komponen Publisher Time Picker </div>
+        )}
+        {selectedPublisher === "Light" && (
+          <div>ini Komponen Publisher Light </div>
+        )}
+        {selectedPublisher === "Fan" && (
+          <div>ini Komponen Publisher Fan </div>
+        )}
+        {selectedPublisher === "Motor" && (
+          <div>ini Komponen Publisher Motor </div>
+        )}
+        {selectedPublisher === "Publisher Device IoT" && (
+          <div>ini Komponen Publisher PUblisher Device IoT </div>
+        )}
+        <div className="bg-purple-400">
           <p>ini selectedSubscriber: {selectedSubscriber}</p>
           <p>ini selectedPublisher: {selectedPublisher}</p>
         </div>
--- src/app/hooks/publisherOptions.ts
+++ src/app/hooks/publisherOptions.ts
@@ -1,1 +1,1 @@
-export const publisherOptions =["Text","Button","Switch","Seek Bar","Combo Box",];
+export const publisherOptions =["Text","Button","Switch","Seek Bar","Combo Box","Color Picker", "Time Picker", "Light", "Fan", "Motor", "Publisher Device IoT"];

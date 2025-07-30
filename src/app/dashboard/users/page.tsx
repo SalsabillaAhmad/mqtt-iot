edefff3a694c9914edc8d854e76ab757b1e8dd46
SalsabillaAhmad <kellycielly22@gmail.com>
2025-07-30 21:49:09 +0700

    membuat caed form configuration untuk component subscriber dan publisher

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -6,6 +6,20 @@ import React from "react";
 import AddButtons from "@/component/ui/AddButtons";
 import ButtonSubscriber from "@/component/ui/ButtonsSubscriber";
 import ButtonPublisher from "@/component/ui/ButtonPublisher";
+import SubscriberWeb from "@/component/cardFormConfiguration/SubscriberWeb";
+import SubscriberDeviceIoT from "@/component/cardFormConfiguration/SubscriberDeviceIoT";
+import Text from "@/component/cardFormConfiguration/Text";
+import Button from "@/component/cardFormConfiguration/Button";
+import Switch from "@/component/cardFormConfiguration/Switch";
+import SeekBar from "@/component/cardFormConfiguration/SeekBar";
+import ComboBox from "@/component/cardFormConfiguration/ComboBox";
+import ColorPicker from "@/component/cardFormConfiguration/ColorPicker";
+import TimePicker from "@/component/cardFormConfiguration/TimePicker";
+import Light from "@/component/cardFormConfiguration/ColorPicker";
+import Fan from "@/component/cardFormConfiguration/Fan";
+import Motor from "@/component/cardFormConfiguration/Motor";
+import PublisherDeviceIoT from "@/component/cardFormConfiguration/PublisherDeviceIoT";
+
 
 export default function UsersDashboard() {
   const {
@@ -34,42 +48,59 @@ export default function UsersDashboard() {
           />
         )}
         {selectedSubscriber === "Subscriber Web" && (
-          <div>ini Komponen Subscriber web</div>
+          // <div className="bg-white p-4 rounded shadow mb-4 max-w-d">
+          //   <h2>Tambah{selectedSubscriber}</h2>
+          // </div>
+          <SubscriberWeb />
         )}
         {selectedSubscriber === "Subscriber Device IoT" && (
-          <div>ini Komponen Subscriber Device IoT</div>
+          // <div>ini Komponen Subscriber Device IoT</div>
+          <SubscriberDeviceIoT />
         )}
-        {selectedPublisher === "Text" && 
-        <div>ini Komponen Publisher Text</div>}
+        {selectedPublisher === "Text" && (
+          // <div>ini Komponen Publisher Text</div>
+          <Text />
+        )}
         {selectedPublisher === "Button" && (
-          <div>ini Komponen Publisher Button</div>
+          // <div>ini Komponen Publisher Button</div>
+          <Button />
         )}
         {selectedPublisher === "Switch" && (
-          <div>ini Komponen Publisher Switch</div>
+          // <div>ini Komponen Publisher Switch</div>
+          <Switch />
         )}
         {selectedPublisher === "Seek Bar" && (
-          <div>ini Komponen Publisher Seek bar </div>
+        //  src/component/cardFormConfiguration/Text.tsx
+           <SeekBar />
         )}
         {selectedPublisher === "Combo Box" && (
-          <div>ini Komponen Publisher Combo Box </div>
+          // src/component/cardFormConfiguration/Text.tsx
+            <ComboBox />
         )}
         {selectedPublisher === "Color Picker" && (
-          <div>ini Komponen Publisher Color Picker </div>
+          // <div>ini Komponen Publisher Color Picker </div>
+          <ColorPicker />
         )}
         {selectedPublisher === "Time Picker" && (
-          <div>ini Komponen Publisher Time Picker </div>
+          // <div>ini Komponen Publisher Time Picker </div>
+          <TimePicker />
         )}
         {selectedPublisher === "Light" && (
-          <div>ini Komponen Publisher Light </div>
+          // <div>ini Komponen Publisher Light </div>
+          <Light />
         )}
         {selectedPublisher === "Fan" && (
-          <div>ini Komponen Publisher Fan </div>
+        // <div>ini Komponen Publisher Fan </div>
+        <Fan />
         )}
+        
         {selectedPublisher === "Motor" && (
-          <div>ini Komponen Publisher Motor </div>
+          // <div>ini Komponen Publisher Motor </div>
+          <Motor />
         )}
         {selectedPublisher === "Publisher Device IoT" && (
-          <div>ini Komponen Publisher PUblisher Device IoT </div>
+          // <div>ini Komponen Publisher PUblisher Device IoT </div>
+          <PublisherDeviceIoT />
         )}
         <div className="bg-purple-400">
           <p>ini selectedSubscriber: {selectedSubscriber}</p>
---
+++ src/component/cardFormConfiguration/ComboBox.tsx
@@ -0,0 +1,20 @@
+export default function ComboBox() {
+  return (
+    <div className="bg-white p-4 rounded shadow mb-4 max-w-md">
+      <h2>Tambah{"Publisher"}</h2>
+      <input
+        type="text"
+        placeholder="device name"
+        className="mb-2 w-full p-2 border border-gray-300 rounded"
+        value={""}
+      />
+      <input
+        type="text"
+        placeholder="topic"
+        className="mb-2 w-full p-2 border border-gray-300 rounded"
+        value={""}
+      />
+
+    </div>
+  );
+}
